b97b86e5d8885d800b0a9eaaa417cbeb2ecf7fd8
Roberto D'Auria <roberto@measurementlab.net>
2024-01-16 19:14:44 +0100

    Add CI workflows and improve build process (#9)
    * Update package/package-lock.json
    
    * Add webpack build GH workflow
    
    * Merge remote-tracking branch 'origin/build-and-publish' into ci
    
    * Run wf on each push or pr
    
    * Add publish workflow
    
    * Add prepare step to package.json
    
    * Provide both minified and unminified umd lib under dist/
    
    * Change babelrc format, run babel in npm script
    
    * Ignore lib/ in .gitignore
    
    * Update package/package-lock
    
    * git ignores lib/, npm does not
    
    * Add comments in .npmignore

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
 const webpack = require('webpack');
 const path = require('path');
+const TerserPlugin = require("terser-webpack-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
+
 const config = {
-  entry: './src/msak.js',
+  entry: {
+    'msak.js': './src/msak.js',
+    'msak.min.js': './src/msak.js',
+  },
   output: {
+    // Each entry corresponds to an output file under dist/
     path: path.resolve(__dirname, 'dist'),
-    filename: 'msak.js',
+    filename: '[name]',
     library: {
       type: 'umd',
       name: 'msak',
@@ -14,6 +20,16 @@ const config = {
     globalObject: 'this',
   },
   devtool: 'source-map',
+  optimization: {
+    minimize: true,
+    minimizer: [
+      // Only minify entries ending in .min.js. This allows to have both the
+      // minified and non-minified versions under dist/.
+      new TerserPlugin({
+        include: /\.min\.js$/
+      })
+    ]
+  },
   module: {
     rules: [
       {
